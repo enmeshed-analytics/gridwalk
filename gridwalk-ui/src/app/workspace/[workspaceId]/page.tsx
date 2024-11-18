c8bfa1f92411f1d19398df0ffb2629de5691d3e4
zerj9 <96551236+zerj9@users.noreply.github.com>
2024-11-18 21:36:37 +0000

    fix: type error

--- gridwalk-ui/src/app/workspace/[workspaceId]/page.tsx
+++ gridwalk-ui/src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,13 @@
 import { getProjectsServer } from "./actions";
 import WorkspaceProjectsClient from "./workspaceProjects";
 
-export default async function Page({
-  params,
-}: {
-  params: { workspaceId: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+type PageProps = {
+  params: Promise<{ workspaceId: string }>;
+};
+
+export default async function Page({ params }: PageProps) {
   const { workspaceId } = await params;
-
   const initialProjects = await getProjectsServer(workspaceId);
-
   return (
     <WorkspaceProjectsClient
       workspaceId={workspaceId}
